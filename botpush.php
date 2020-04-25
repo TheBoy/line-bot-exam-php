90bd0c07cef1a14848600f8b71d6d7c107ea1037
TheBoyz <walanchai@live.com>
2020-04-26 03:00:51 +0700

    cfg

--- botpush.php
+++ botpush.php
@@ -3,9 +3,8 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 require "vendor/autoload.php";
+include "cfg.inc.php";
 
-$access_token = 'kzqzlfgem1Lz5p1c+YCdemj3CPoIQNgYAl2H8l5xBiolWxSwEkQAt928oav0MwNAJZFSPvcRaCxEsbFx9YMVdljG2vybayRKjyMrxaiLt+7gBuX2a+sNRmURstLtkRE4wRgmM6mo1EbhTvlnUs9CHQdB04t89/1O/w1cDnyilFU=';
-$channelSecret = '4e47cd8160b5b54c68e9de710e8647af';
 $pushID = 'Ud766ebb32103f4f5962851494f23b918';
 
 $text = isset($_GET['text']) ? $_GET['text'] : NULL;

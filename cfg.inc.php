0569a94f0973ce76787e7cfe6bdd07309116e294
TheBoyz <walanchai@live.com>
2020-04-26 03:01:58 +0700

    cfg

---
+++ cfg.inc.php
@@ -0,0 +1,8 @@
+<?php
+$channel_id = '1561474156';
+$channel_secret = '4e47cd8160b5b54c68e9de710e8647af';
+$access_token = 'kzqzlfgem1Lz5p1c+YCdemj3CPoIQNgYAl2H8l5xBiolWxSwEkQAt928oav0MwNAJZFSPvcRaCxEsbFx9YMVdljG2vybayRKjyMrxaiLt+7gBuX2a+sNRmURstLtkRE4wRgmM6mo1EbhTvlnUs9CHQdB04t89/1O/w1cDnyilFU=';
+
+define('LINE_MESSAGE_CHANNEL_ID', $channel_id);
+define('LINE_MESSAGE_CHANNEL_SECRET', $channel_secret);
+define('LINE_MESSAGE_ACCESS_TOKEN', $access_token);
